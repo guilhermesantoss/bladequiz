a5148cd2e79149e6764eb624d2041b29b1e035a7
Guilherme Santos <g.santos.sfc@gmail.com>
2021-01-25 10:56:58 -0300

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
+import Head from 'next/head';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -26,28 +27,50 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+    <>
+      <Head>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Quiz Rocket League</title>
+        <meta name="title" content="Quiz Rocket League" />
+        <meta name="description" content="Página criada durante a semana da Alura Imersão React v2 feita com Next.js" />
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://bladequiz.vercel.app/" />
+        <meta property="og:title" content="Quiz Rocket League" />
+        <meta property="og:description" content="Página criada durante a semana da Alura Imersão React v2 feita com Next.js" />
+        <meta property="og:image" content="" />
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://bladequiz.vercel.app/" />
+        <meta property="twitter:title" content="Quiz Rocket League" />
+        <meta property="twitter:description" content="Página criada durante a semana da Alura Imersão React v2 feita com Next.js" />
+        <meta property="twitter:image" content="" />
+      </Head>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
+
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
+
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/guilhermesantoss" />
-    </QuizBackground>
-  );
+      </QuizBackground>
+    </>
+);
 }
